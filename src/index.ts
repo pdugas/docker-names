58fc69e5ed36ae889c0c96be8e6d8524ce3b019d
Paul Dugas <pdugas@cribl.io>
2025-09-17 13:39:51 -0400

    speling is hard

--- src/index.ts
+++ src/index.ts
@@ -827,7 +827,7 @@ export const right = [
     "draese",     // Olli Draese      - Sr Principal Software Engineer
     "dryden",     // Cam Dryden       - Regional Sales Manager
     "duca",       // Duca Duca        - Dir, Professional Services
-    "dugas",      // Paul Dugas       - Principlal Software Engineer
+    "dugas",      // Paul Dugas       - Principal Software Engineer
     "duke",       // David Duke       - Sr Technical Writer
     "dzhumakaliev", // Ruslan Dzhumakaliev - Software Engineer II
     "easley",     // Bryce Easley     - Sr Software Engineer
